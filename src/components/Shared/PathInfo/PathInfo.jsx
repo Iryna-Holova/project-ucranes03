d258c9e714792f31a48f779155c6dab7e96c9266
Iryna Holova <gis.golova@gmail.com>
2024-06-07 11:18:00 +0300

    Update PathInfo.jsx

--- src/components/Shared/PathInfo/PathInfo.jsx
+++ src/components/Shared/PathInfo/PathInfo.jsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
 import css from './PathInfo.module.css';
 
-const PathInfo = () => {
-  return <div className={css.path}>PathInfo</div>;
+const PathInfo = ({ current }) => {
+  return (
+    <div className={css.path}>
+      <Link to="/">Home</Link>
+      <span>/</span>
+      <span className={css.current}>{current}</span>
+    </div>
+  );
 };
 
 export default PathInfo;
