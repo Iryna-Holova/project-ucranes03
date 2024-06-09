de8cc9e19c99c3314eed204772faa527f812c8d9
Maxus94 <kmaxusk@gmail.com>
2024-06-09 15:09:00 +0300

    create categories

--- src/components/Categories/CategoryList/CategoryList.jsx
+++ src/components/Categories/CategoryList/CategoryList.jsx
@@ -1,5 +1,26 @@
+import categoriesList from '../../../temp/categories.json';
+import css from './CategoryList.module.css';
+import sprite from '../../../../src/images/icons.svg'
+
 const CategoryList = () => {
-  return <ul>CategoryList</ul>;
+  return (
+    <ul className={css.categoriesList}>
+      {categoriesList.map(category => (
+        <li className={css.categoryItem}>
+          <div className={css.categoryLinkContainer}>
+            <a className={css.categoryLink} href="">
+              {category.name}
+            </a>
+            <a className={css.categoryArrow} href="">
+              <svg width="18" height="18">
+                <use href={sprite + '#icon-arrow-up-right'}></use>
+              </svg>
+            </a>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default CategoryList;
