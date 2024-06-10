cf51ed04aac99ffb2d0ea61f88fa0a557dceef20
filimon4uck <filimon4uck.yura@gmail.com>
2024-06-10 22:21:57 +0200

    added component SelectFilter, added components SelectFilter to RecipeFilters

--- src/components/Recipes/RecipeFilters/RecipeFilters.jsx
+++ src/components/Recipes/RecipeFilters/RecipeFilters.jsx
@@ -1,5 +1,62 @@
+import axios from "axios";
+import SelectFilter from "components/Shared/SelectFilter/SelectFilter";
+import { useEffect, useState } from "react";
+import { ingredients, areas } from "temp/ingredients";
 const RecipeFilters = () => {
-  return <div>RecipeFilters</div>;
+  const [areas, setAreas] = useState((areas) => areas.map(area => ({ value: area._id.$oid, label: area.name }) );
+  useEffect(() => {
+    const fetchIngredients = async () => {
+      try {
+        const response = await axios.get('');
+        console.log(response)
+        setAreas(response.data);
+      } catch (error) {
+        console.error('Error fetching the ingredients:', error);
+      }
+    };
+
+    fetchIngredients();
+  }, [])
+  const [selectedOptions, setSelectedOptions] = useState({
+    ingredients: null,
+    areas: null,
+  });
+
+  const handleChange = (option, { name }) => {
+    setSelectedOptions((prevOptions) => ({
+      ...prevOptions,
+      [name]: option,
+    }));
+  };
+  const options1 = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+  ]
+  const options2 = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
+  return (
+    <div>
+      <h1>Custom Select Component</h1>
+      <SelectFilter
+        name="ingredients"
+        options={options1}
+        onChange={handleChange}
+        value={selectedOptions.ingredients}
+        placeholder="ingredients"
+      />
+      <SelectFilter
+        name="category"
+        options={options2}
+        onChange={handleChange}
+        value={selectedOptions.category}
+        placeholder="areas"
+      />
+    </div>
+  );
 };
 
 export default RecipeFilters;
---
+++ src/components/Shared/SelectFilter/SelectFilter.jsx
@@ -0,0 +1,15 @@
+import Select from 'react-select';
+
+const SelectFilter =({ options, onChange, value, placeholder, name }) => {
+  return (
+    <Select
+      options={options}
+      isSearchable
+      isClearable
+      onChange={(option) => onChange(option, { name })}
+      value={value}
+      placeholder={placeholder}
+    />
+  );
+};
+export default SelectFilter;
