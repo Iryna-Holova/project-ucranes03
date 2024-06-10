78a4aae4a73bf208774c081d84162c4148baf1ff
mazurkevych30 <132198562+mazurkevych30@users.noreply.github.com>
2024-06-10 22:56:12 +0100

    add markup for recipe info

--- src/components/RecipeInfo/RecipeMainInfo/RecipeMainInfo.jsx
+++ src/components/RecipeInfo/RecipeMainInfo/RecipeMainInfo.jsx
@@ -1,5 +1,22 @@
-const RecipeMainInfo = () => {
-  return <div>RecipeMainInfo</div>;
+const RecipeMainInfo = ({ thumb, title, category, time, description }) => {
+  return (
+    <div>
+      <img src={thumb} alt={title} />
+      <h2>{title}</h2>
+      <ul>
+        <li>{category}</li>
+        <li>{time}</li>
+      </ul>
+      <p>{description}</p>
+      <button>
+        <img src="" alt="" />
+        <span>
+          <span>Created by:</span>
+          <span></span>
+        </span>
+      </button>
+    </div>
+  );
 };
 
 export default RecipeMainInfo;
