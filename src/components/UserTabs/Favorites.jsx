1b4a444a3f2b7daf47ebd5fb08f53ca17b7015f9
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-06-10 12:07:48 +0300

    added markup, styles, some logic

--- src/components/UserTabs/Favorites.jsx
+++ src/components/UserTabs/Favorites.jsx
@@ -1,12 +1,13 @@
 import ListItems from 'components/UserTabs/ListItems/ListItems';
 import RecipePreview from 'components/UserTabs/RecipePreview/RecipePreview';
+import { favoriteRecipes } from 'temp/recipeData';
 
 const Favorites = () => {
   return (
     <div>
       <h3 className="visually-hidden">Favorite recipes</h3>
       <ListItems />
-      <RecipePreview />
+      <RecipePreview recipes={favoriteRecipes} />
     </div>
   );
 };
