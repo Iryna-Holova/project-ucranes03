bb29685e5bfaf789be1205f2404f76ad06643dc2
Maxus94 <kmaxusk@gmail.com>
2024-06-10 09:50:46 +0300

    correct categories

--- src/components/Categories/CategoryList/CategoryList.jsx
+++ src/components/Categories/CategoryList/CategoryList.jsx
@@ -1,21 +1,24 @@
 import categoriesList from '../../../temp/categories.json';
 import css from './CategoryList.module.css';
-import sprite from '../../../../src/images/icons.svg'
+import sprite from '../../../images/icons.svg'
+import { Link } from 'react-router-dom';
 
 const CategoryList = () => {
   return (
-    <ul className={css.categoriesList}>
+    <ul className={css.categories_list}>
       {categoriesList.map(category => (
-        <li className={css.categoryItem}>
-          <div className={css.categoryLinkContainer}>
-            <a className={css.categoryLink} href="">
+        <li key={category._id} className={css.category_item}>
+          <div className={css.category_link_container}>
+          <Link className={css.category_link} to={`recipes/?category=${category.name}`}>
               {category.name}
-            </a>
-            <a className={css.categoryArrow} href="">
+          </Link>
+          <Link className={css.category_arrow} to={`recipes/?category=${category.name}`}>
+            {/* <a className={css.category_arrow} href=""> */}
               <svg width="18" height="18">
                 <use href={sprite + '#icon-arrow-up-right'}></use>
               </svg>
-            </a>
+            {/* </a> */}
+            </Link>
           </div>
         </li>
       ))}
