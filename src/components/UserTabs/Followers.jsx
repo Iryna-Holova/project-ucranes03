5bde849fb966e15df311c409e3aae984f2dcefc0
Maxim <kmaxusk@gmail.com>
2024-06-12 17:05:15 +0300

    start

--- src/components/UserTabs/Followers.jsx
+++ src/components/UserTabs/Followers.jsx
@@ -1,11 +1,16 @@
 import ListItems from 'components/UserTabs/ListItems/ListItems';
 import users from '../../temp/users.json';
+import UserCard from './UserCard/UserCard';
 
-const Followers = () => {  
+const Followers = () => {
   return (
     <div>
-      <h3 className="visually-hidden">Followers</h3>      
-      <ListItems users = {users.results} tabType ="followers"/>
+      <h3 className="visually-hidden">Followers</h3>
+      <ListItems>
+        {users.map(user => (
+          <UserCard key={user.id} user={user} tabType="folowers" />
+        ))}
+      </ListItems>
       {/* remove tabType */}
     </div>
   );
--- src/components/UserTabs/ListItems/ListItems.jsx
+++ src/components/UserTabs/ListItems/ListItems.jsx
@@ -1,16 +1,16 @@
 // import UserCard from '../UserCard/UserCard';
 
-const ListItems = ({ users, tabType }) => {
-  return (
-    <ul>
-      {/* {users.map((user) => (
-        <UserCard key={user.id} user={user} tabType = {tabType} />
-      ))} */}
-    </ul>
-  );
+const ListItems = ({ children }) => {
+  return <ul>{children}</ul>;
 };
 
 export default ListItems;
 
 // {id, name, avatar_preview, recipes}
 // name={name} avatar_preview={avatar_preview} recipes={recipes}
+
+{
+  /* {users.map((user) => (
+        <UserCard key={user.id} user={user} tabType = {tabType} />
+      ))} */
+}
