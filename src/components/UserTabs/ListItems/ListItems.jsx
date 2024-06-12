365bf6d95eaa714a7c200bea7019e464ee03b3bc
Maxus94 <kmaxusk@gmail.com>
2024-06-12 20:32:40 +0300

    repair structure

--- src/components/UserTabs/ListItems/ListItems.jsx
+++ src/components/UserTabs/ListItems/ListItems.jsx
@@ -1,14 +1,5 @@
-// import UserCard from '../UserCard/UserCard';
-
 const ListItems = ({ children }) => {
   return <ul>{children}</ul>;
 };
 
 export default ListItems;
-
-// {id, name, avatar_preview, recipes}
-// name={name} avatar_preview={avatar_preview} recipes={recipes}
-
-  //  {users.map((user) => (
-  //       <UserCard key={user.id} user={user} tabType = {tabType} />
-  //     ))} 
