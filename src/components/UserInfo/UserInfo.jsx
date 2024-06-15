2a605e5591dba9a16803c961336152de4763f842
filimon4uck <filimon4uck.yura@gmail.com>
2024-06-15 15:25:06 +0200

    added userInfoservices, added user info logic

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,57 +1,103 @@
+
 import ButtonLink from 'components/Shared/ButtonLink/ButtonLink';
+import Image from 'components/Shared/Image/Image';
 import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { getOwnInfo, getCurrentInfo } from 'services/userInfo';
-// import css from "./UserInfo.module.css";
+import { getOwnInfo, getInfo, updateAvatar } from 'services/userInfo';
+import defaultAvatar from 'images/placeholder-avatar.svg';
+import icons from 'images/icons.svg';
+import css from './UserInfo.module.css';
+import { setAvatar } from 'store/authSlice/slice';
+import Modal from 'components/Modal/Modal';
+import LogOutModal from 'components/LogOutModal/LogOutModal';
+
+
 
 const UserInfo = () => {
-  const params = useParams();
-  const [isCurrent, setIsCurrent] = useState(false);
-  const [userData, setUserData] = useState(null);
 
+  const dispatch = useDispatch();
+  const user = useParams().id;
+  const isCurrent = user === 'current';
+  const [userData, setUserData] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        if (params.id === "current") {
+        if (isCurrent) {
           const { data } = await getOwnInfo();
           setUserData(data);
-          setIsCurrent(true);
         } else {
-          const { data } = await getCurrentInfo(params.id);
+          const { data } = await getInfo(user);
           setUserData(data);
-          setIsCurrent(false);
         }
       } catch (error) {
         console.log(error);
       }
     };
     fetchData();
-  }, [params]);
+  }, [isCurrent, user]);
+  
+  const handleFileChange = async(event) => {
+    
+    const file = event.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append('avatar', file);
+      try {
+        const { data } = await updateAvatar(formData);
+        setUserData(prevData => ({ ...prevData, avatar: data.avatar }));
+        dispatch(setAvatar(data.avatar));
+    }
+    catch (error) { 
+      console.log(error)
+    }
 
-  const { email, recipes, favorites, followers, following, name } = userData || {};
+    }
+  };
+
+  const { email, recipes, favorites, followers, following, name, avatar } = userData || {};
 
   return (
     <>
-      {userData ? (
-        <div>
-          <p>{name}</p>
-          <ul>
-            <li><span>Email</span>: <span>{email}</span></li>
-            <li><span>Added recipes</span>: <span>{recipes}</span></li>
-            {isCurrent && <li><span>Favorites</span>: <span>{favorites}</span></li>}
-            <li><span>Followers</span>: <span>{followers}</span></li>
-            {isCurrent && <li><span>Following</span>: <span>{following}</span></li>}
-          </ul>
-          <ButtonLink type="button" onClick={() => console.log('Log out clicked')}>
+      <Image publicId={avatar} alt="avatar" defaultImage={defaultAvatar} className={css.avatar} />
+      { isCurrent&&
+      <div className={css.fileInputWrapper}>
+         <input
+          className={css.input_file}
+          type="file"
+          onChange={handleFileChange}
+        />
+        <svg className={css.icon_plus}>
+          <use href={`${icons}#icon-plus`}></use>
+        </svg>
+      </div>}
+      <div>
+        <p>{name}</p>
+        <ul>
+          <li><span>Email</span>: <span>{email}</span></li>
+          <li><span>Added recipes</span>: <span>{recipes}</span></li>
+          {isCurrent && <li><span>Favorites</span>: <span>{favorites}</span></li>}
+          <li><span>Followers</span>: <span>{followers}</span></li>
+          {isCurrent && <li><span>Following</span>: <span>{following}</span></li>}
+        </ul>
+        {isCurrent && (
+          <ButtonLink type="button" onClick={() =>setIsModalOpen(true)}>
             Log out
           </ButtonLink>
+        )}
+        {!isCurrent && (
           <ButtonLink type="button" onClick={() => console.log('Follow clicked')}>
             Follow
           </ButtonLink>
-        </div>
-      ) : (
-        <p>Loading...</p>
+          
+        )}
+        {isModalOpen && (
+        <Modal onClose={() => setIsModalOpen(false)}>
+          <LogOutModal onClose={() => setIsModalOpen(false)} />
+        </Modal>
       )}
+      </div>
     </>
   );
 };
