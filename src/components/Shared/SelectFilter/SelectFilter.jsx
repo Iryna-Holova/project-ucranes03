e76bcf06fa7f7934c8b902aedeaa3b308690ffae
filimon4uck <filimon4uck.yura@gmail.com>
2024-06-16 13:33:04 +0200

    added custom styles

--- src/components/Shared/SelectFilter/SelectFilter.jsx
+++ src/components/Shared/SelectFilter/SelectFilter.jsx
@@ -1,15 +1,19 @@
+import React from 'react';
 import Select from 'react-select';
+import { customStyles } from './customStyles';
 
-const SelectFilter =({ options, onChange, value, placeholder, name }) => {
+const SelectFilter = ({ options, onChange, value, placeholder, name }) => {
   return (
     <Select
       options={options}
       isSearchable
       isClearable
-      onChange={(option) => onChange(option, { name })}
+      styles={customStyles}
+      onChange={option => onChange(option, { name })}
       value={value}
       placeholder={placeholder}
-      /> 
+    />
   );
 };
+
 export default SelectFilter;
