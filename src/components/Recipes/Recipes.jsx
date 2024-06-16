75e4343d0fa68577241e44148c45f625169891c0
Iryna Holova <gis.golova@gmail.com>
2024-06-16 14:38:01 +0300

    final recipes

--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useRef, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useSearchParams } from 'react-router-dom';
+import { fetchCategories } from 'store/categoriesSlice/thunks';
 import { selectActiveCategory } from 'store/categoriesSlice/selectors';
 import { useMobileMediaQuery } from 'hooks/device-type';
 import { getRecipes } from 'services/recipes';
+import { scrollToElement } from 'helpers/scolls';
+import { showError } from 'helpers/notification';
 import MainTitle from 'components/Shared/MainTitle/MainTitle';
 import Subtitle from 'components/Shared/Subtitle/Subtitle';
 import RecipeFilters from './RecipeFilters/RecipeFilters';
 import RecipeList from 'components/RecipeList/RecipeList';
+import RecipeCard from 'components/RecipeList/RecipeCard/RecipeCard';
+import RecipeCardSkeleton from 'components/RecipeList/RecipeCard/RecipeCardSkeleton';
 import Pagination from 'components/Shared/Pagination/Pagination';
 import icons from 'images/icons.svg';
 import css from './Recipes.module.css';
@@ -16,14 +21,22 @@ const Recipes = () => {
   const [params] = useSearchParams();
   const [recipes, setRecipes] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
   const topElementRef = useRef(null);
   const isMobile = useMobileMediaQuery();
+
   const category = useSelector(state =>
     selectActiveCategory(state, params.get('category'))
   );
 
   useEffect(() => {
+    if (!category) dispatch(fetchCategories());
+  }, [category, dispatch]);
+
+  useEffect(() => {
     const fetchRecipes = async () => {
+      setIsLoading(true);
       try {
         const limit = isMobile ? 8 : 12;
         const page = params.get('page') || 1;
@@ -40,16 +53,20 @@ const Recipes = () => {
         setRecipes(data.results);
         setTotalPages(data.totalPages);
       } catch (error) {
-        throw Error(error.message);
+        showError(error);
+      } finally {
+        setIsLoading(false);
       }
     };
+    scrollToElement(topElementRef.current);
     fetchRecipes();
   }, [isMobile, params]);
 
   return (
     <section className="section">
+      <div ref={topElementRef}></div>
       <div className={css.header}>
-        <Link to="/" className={css.back_link} ref={topElementRef}>
+        <Link to="/" className={css.back_link}>
           <svg width={16} height={16}>
             <use href={`${icons}#icon-arrow-left`} />
           </svg>
@@ -64,7 +81,15 @@ const Recipes = () => {
       <div className={css.content}>
         <RecipeFilters />
         <div>
-          <RecipeList recipes={recipes} />
+          <RecipeList recipes={recipes}>
+            {isLoading
+              ? [...Array(isMobile ? 8 : 12)].map((_, idx) => (
+                  <RecipeCardSkeleton key={idx} />
+                ))
+              : recipes.map(recipe => (
+                  <RecipeCard key={recipe._id} recipe={recipe} />
+                ))}
+          </RecipeList>
           {totalPages > 1 && (
             <Pagination
               totalPages={totalPages}
