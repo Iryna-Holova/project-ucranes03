978c04c562c070cec45ce40c17dbbb90ef890c4c
Maxus94 <kmaxusk@gmail.com>
2024-06-16 17:05:37 +0300

    create footer

--- src/components/Shared/Logo/Logo.jsx
+++ src/components/Shared/Logo/Logo.jsx
@@ -1,11 +1,13 @@
-import { useLocation } from "react-router-dom";
-import { Link } from "react-router-dom";
-import css from "./Logo.module.css";
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import css from './Logo.module.css';
 
-const Logo = () => {
-  const pagesArray = ["/", `/recipes`];
+const Logo = ({ inFooter = false }) => {
+  const pagesArray = ['/', `/recipes`];
   const location = useLocation().pathname;
-  const isBlackTheme = !pagesArray.some((page) => page === location);
+  const isBlackTheme = !inFooter
+    ? !pagesArray.some(page => page === location)
+    : pagesArray.some(page => page === location);
 
   return (
     <Link to="/" className={isBlackTheme ? css.logo_black : css.logo_white}>
