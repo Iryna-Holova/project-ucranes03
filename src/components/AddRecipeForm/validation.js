287ae44a4f70f8bd0b969c545b8c2f5d9f5ca31b
filimon4uck <filimon4uck.yura@gmail.com>
2024-06-17 20:22:25 +0200

    fixed some bugs in add form

---
+++ src/components/AddRecipeForm/validation.js
@@ -0,0 +1,51 @@
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const schema = yup.object().shape({
+  thumb: yup
+    .mixed()
+    .required('Photo is required')
+    .test('fileType', 'Unsupported File Format', value => {
+      return value && value[0]?.type.split('/')[0] === 'image';
+    }),
+  title: yup
+    .string()
+    .required('Title is required')
+    .min(3, 'Title must have at least 3 symbols')
+    .max(30),
+  description: yup
+    .string()
+    .required('Description is required')
+    .max(200, 'Description should not exceed 200 characters'),
+  category: yup
+    .object()
+    .shape({
+      value: yup.string().required('Category is required'),
+    })
+    .required('Category is required'),
+  area: yup
+    .object()
+    .shape({
+      value: yup.string().required('Area is required'),
+    })
+    .required('Area is required'),
+  ingredientsList: yup
+    .array()
+    .of(
+      yup.object().shape({
+        id: yup.string().required('Ingredient is required'),
+        measure: yup.string().required('Measure is required'),
+      })
+    )
+    .min(1, 'At least one ingredient is required'),
+  instructions: yup
+    .string()
+    .required('Instructions are required')
+    .max(2500, 'Instructions should not exceed 2500 characters'),
+  time: yup
+    .number()
+    .required('Time is required')
+    .min(1, 'Time should be at least 1 minute'),
+});
+
+export const resolver = yupResolver(schema);
